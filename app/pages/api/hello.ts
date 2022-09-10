6a73faa5c1d975e7fccccd7fdd3f78f079c8af67
ichir0roieHome <ichir0roie.com>
2022-09-10 20:08:59 +0900

    reset

--- app/pages/api/hello.ts
+++ app/pages/api/hello.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+
 type Data = {
   name: string
 }
@@ -9,5 +10,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // var name=req.query["name"]!==undefined?req.query["name"][0]:""
+  // res.status(200).json({ name:name })
+  res.status(200).json(req.query)
 }
